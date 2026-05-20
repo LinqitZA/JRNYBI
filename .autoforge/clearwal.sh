98e6dfb08a9f142dd7a2a54dfc8ff8089735ded1
Aadil Aboobaker <aadil@linqit.co.za>
2026-05-20 05:05:16 +0200

    schema

---
+++ .autoforge/clearwal.sh
@@ -0,0 +1,2 @@
+sqlite3 features.db "PRAGMA wal_checkpoint(FULL);"
+sqlite3 features.db "PRAGMA wal_checkpoint(TRUNCATE);"
---
+++ scripts/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  baseHref: "/bi/",
+  staticPath: "/bi/static/"
+};
